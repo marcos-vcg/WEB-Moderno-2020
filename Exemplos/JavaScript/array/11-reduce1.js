3b2786288db51d63cd9d81c83466d03842e628a2
Marcos Costa <marcos.vcg.ads@gmail.com>
2020-06-07 19:55:32 -0300

    criado reduce

---
+++ Exemplos/JavaScript/array/11-reduce1.js
@@ -0,0 +1,16 @@
+const alunos = [
+    {nome: 'João', nota: 7.3, bolsista: false},
+    {nome: 'Maria', nota: 9.2, bolsista: true},
+    {nome: 'Pedro', nota: 9.8, bolsista: false},
+    {nome: 'Ana', nota: 8.7, bolsista: true}
+]
+
+// Aqui é acumulado um valor no primeiro elemento do Array e somando ao valor do segundo elemento do array
+// Isso percorrendo todo o Array.
+console.log(alunos.map(a => a.nota))
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual) {
+    console.log(acumulador, atual)
+    return acumulador + atual
+}, 0)
+
+console.log(resultado)
